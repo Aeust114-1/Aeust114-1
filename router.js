d9c19349d4bc10d92c871c699038136c18bc2367
Carnot-maker <hankasd46413149@gmail.com>
2025-12-29 22:46:26 +0800

    DB、登入、註冊

--- router.js
+++ router.js
@@ -2,31 +2,44 @@
 import * as controller from './modules/userController.js'; // 把所有功能引入並取名為 controller
 
 function router(req, res) {
-    const { url, method } = req;
 
-    switch (url) {
+
+
+
+    switch (req.url) {
         case '/':
             // 首頁直接導向登入
             res.writeHead(302, { 'Location': '/login' });
             res.end();
             break;
 
-        case '/login':
-            if (method === 'GET') {
+        case '/login'://登入頁
+            if (req.method === 'GET') {//顯示登入頁
                 controller.showLogin(res);
-            } else if (method === 'POST') {
+            } else if (req.method === 'POST') {//處理登入邏輯
                 controller.handleLogin(req, res);
             }
             break;
-
-        case '/dashboard':
-            if (method === 'GET') {
-                controller.showDashboard(res);
+        
+        case '/register':
+            if (req.method === 'GET') {
+                // 如果是用戶想看註冊表單
+                controller.showRegister(res);
+            } else if (req.method === 'POST') {
+                // 如果是用戶送出註冊資料
+                controller.handleRegister(req, res);
             }
             break;
 
-        default:
-            controller.show404(res);
+        case '/dashboard'://人員管理系統
+            if (req.method === 'GET') {
+                controller.showDashboard(res);
+            }
+            break;
+        
+
+        default://傳出錯誤頁面
+            controller.Error404(res);
             break;
     }
 }
